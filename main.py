8153cfaad7d7b14a99c01e950b9100b8119eb38f
Josh Bolipata <joshtiffton@gmail.com>
2022-03-03 20:15:25 +0800

    changed window title

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
-
+        self.setWindowTitle("To-Do List")
         self.addItem.clicked.connect(self.add_it)
         self.deleteItem.clicked.connect(self.delete_it)
         self.clear.clicked.connect(self.clear_list)
-
+        self.save.clicked.connect(self.save_list)
         self.show()
     
+    def keyPressEvent(self, event):
+       if event.key() == QtCore.Qt.Key_Return:
+           self.add_it()
+
+    def save_list(self):
+        items = []
+        for i in range(self.list.count()):
+            items.append(self.list.item(i))
+
+        for item in items:
+            print(item.text())
+
     def add_it(self):
         item = self.input.text()
         if item != "":
